67c6977e062469d17ff46ba0569e5c2149deb273
Aaron Isaac <aaron@aisaac.co>
2024-06-21 06:09:20 +0700

    Add basic filtering

---
+++ src/main/java/co/aisaac/upwork/MainFilter.java
@@ -0,0 +1,11 @@
+package co.aisaac.upwork;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MainFilter {
+    String status = "new";
+    String searchTerms = "";
+}
