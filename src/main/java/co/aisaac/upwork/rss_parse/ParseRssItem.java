8c2679a6841daf1243f3aab1b1b6c5cfab8560a5
Aaron Isaac <aaron@aisaac.co>
2024-06-20 21:56:04 +0700

    Finish the parser

---
+++ src/main/java/co/aisaac/upwork/rss_parse/ParseRssItem.java
@@ -0,0 +1,92 @@
+package co.aisaac.upwork.rss_parse;
+
+import co.aisaac.upwork.Posting;
+import com.apptasticsoftware.rssreader.Item;
+import org.jsoup.Jsoup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+class ParseRssItem {
+    private static final Logger log = LoggerFactory.getLogger(ParseRssItem.class);
+
+    static Posting parse(Item item) {
+        Posting posting = new Posting();
+        Optional<String> description = item.getDescription();
+
+        if (description.isPresent()) {
+            String t = Jsoup.parse(description.get()).wholeText();
+            String text = t.replaceAll("\n\n", "\n");
+            String[] lines = text.split("\n");
+
+            for (String line : lines) {
+
+                if (line.startsWith("Budget:")) {
+                    posting.budget = line.substring(8).trim();
+                } else if (line.startsWith("Hourly Range:")) {
+                    posting.hourlyRange = line.substring(13).trim();
+                } else if (line.startsWith("Category:")) {
+                    posting.category = line.substring(10).trim();
+                } else if (line.startsWith("Posted On:")) {
+                    try {
+                        posting.datetime = LocalDateTime.parse(line.substring(11).trim());
+                    } catch (Exception ex) {
+                        posting.datetime = LocalDateTime.now();
+                    }
+                } else if (line.startsWith("Country:")) {
+                    posting.country = line.substring(9).trim();
+                } else if (line.startsWith("Skills:")) {
+                    String s = line.substring(7).trim();
+                    String clean = Arrays.stream(s.split(",")).map(String::trim).collect(Collectors.joining(", "));
+                    if (posting.skills != null) {
+                        posting.skills = posting.skills + ", " + clean;
+                    } else {
+                        posting.skills = clean;
+                    }
+                }
+
+            }
+            posting.description = text;
+        } else {
+            log.warn("Unable to find description on RSS feed item.");
+            return null;
+        }
+
+        if (item.getTitle().isPresent()) {
+            posting.title = item.getTitle().get();
+        } else {
+            posting.title = "Unknown title.";
+            log.warn("Unable to find title on RSS feed item.");
+        }
+
+        if (item.getGuid().isPresent()) {
+            posting.guid = item.getGuid().get();
+        } else {
+            log.warn("Unable to find GUID on RSS feed item.");
+            return null;
+        }
+
+        if (item.getPubDate().isPresent()) {
+            try {
+                posting.pubDate = LocalDateTime.parse(item.getPubDate().get());
+            } catch (Exception ex) {
+                posting.pubDate = LocalDateTime.now();
+            }
+        } else {
+            log.warn("Unable to find PubDate on RSS feed item.");
+        }
+
+        if (item.getLink().isPresent()) {
+            posting.url = item.getLink().get();
+        } else {
+            log.warn("Unable to find URL for RSS feed item.");
+            return null;
+        }
+
+        return posting;
+    }
+}
---
+++ src/main/java/co/aisaac/upwork/rss_parse/ParseRssScheduler.java
@@ -0,0 +1,57 @@
+package co.aisaac.upwork.rss_parse;
+
+import co.aisaac.upwork.Posting;
+import co.aisaac.upwork.PostingRepo;
+import com.apptasticsoftware.rssreader.Item;
+import com.apptasticsoftware.rssreader.RssReader;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.IOException;
+
+@Slf4j
+@Configuration
+public class ParseRssScheduler {
+
+    @Autowired
+    PostingRepo postingRepo;
+
+    @Scheduled(fixedDelay = 1 * 60 * 1000)
+    public void run() throws IOException {
+        String url = "https://www.upwork.com/ab/feed/jobs/rss" +
+                "?category2_uid=531770282580668418" +
+                "&contractor_tier=2%2C3&hourly_rate=40-" +
+                "&sort=recency" +
+                "&t=0" +
+                "&api_params=1" +
+                "&securityToken=f2b1425b06ba5e895d96d6a621a0551c9cb3a8bee728999cb662cb574696abcffa77f426df80aff253a7028422fdcda0c91ff972aede682c39c4300e3bed2801" +
+                "&userUid=1299200877346168832" +
+                "&orgUid=1299200877350363137";
+
+        RssReader rssReader = new RssReader();
+
+        for (Item item : rssReader.read(url).toList()) {
+
+            Posting posting = ParseRssItem.parse(item);
+            if(posting == null){
+                log.warn("Unable to parse RSS feed item.");
+                continue;
+            }
+
+            System.out.println(posting);
+            System.out.println("\n");
+
+            if (postingRepo.existsByGuid(posting.guid) == false) {
+                postingRepo.save(posting);
+            }
+        }
+
+        // tomorrow
+        // store postings in database
+        // send emails or do notification when java postings show up
+    }
+
+
+}
