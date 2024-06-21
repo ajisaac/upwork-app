088c5b3d350dcdc9c853d5418eb2b9a4adf23e7f
Aaron Isaac <aaron@aisaac.co>
2024-06-22 03:11:55 +0700

    Add remember me

--- src/main/java/co/aisaac/upwork/security/SecurityConfig.java
+++ src/main/java/co/aisaac/upwork/security/SecurityConfig.java
@@ -1,12 +1,18 @@
 package co.aisaac.upwork.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+
+import static org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices.*;
 
 @Configuration
 @EnableWebSecurity
@@ -15,12 +21,20 @@ public class SecurityConfig {
     @Autowired
     AppUserDetailsService userDetailsService;
 
+    @Value("${application.security.remember_me_key}")
+    String rememberMeKey;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, RememberMeServices rememberMeServices) throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
                         .anyRequest().authenticated()
                 )
+                .rememberMe((remember) -> {
+                    remember.rememberMeServices(rememberMeServices);
+                    remember.alwaysRemember(true);
+                    remember.key(rememberMeKey);
+                })
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults());
 
@@ -28,18 +42,12 @@ public class SecurityConfig {
     }
 
 
-//    @Bean
-//    RememberMeServices rememberMeServices(UserDetailsService userDetailsService) {
-//        RememberMeTokenAlgorithm encodingAlgorithm = RememberMeTokenAlgorithm.SHA256;
-//        TokenBasedRememberMeServices rememberMe = new TokenBasedRememberMeServices(myKey, userDetailsService, encodingAlgorithm);
-//        rememberMe.setMatchingAlgorithm(RememberMeTokenAlgorithm.MD5);
-//        return rememberMe;
-//    }
-
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
+    @Bean
+    RememberMeServices rememberMeServices(UserDetailsService userDetailsService) {
+        RememberMeTokenAlgorithm encodingAlgorithm = RememberMeTokenAlgorithm.SHA256;
+        TokenBasedRememberMeServices rememberMe = new TokenBasedRememberMeServices(rememberMeKey, userDetailsService, encodingAlgorithm);
+        rememberMe.setMatchingAlgorithm(RememberMeTokenAlgorithm.MD5);
+        return rememberMe;
+    }
 
 }
