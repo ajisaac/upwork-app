21458f4e93ad21c1729203619b3158ba0f96743d
Aaron Isaac <aaron@aisaac.co>
2024-06-21 22:48:10 +0700

    Modify deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/zsh
+
+cd src/main/tailwind || exit
+npm run build
+
+cd ../../..
 mvn clean package
 rsync -avz -e "ssh -i ~/.ssh/JobScraperKey.pem" ./target/upwork-0.0.1.jar ubuntu@server-host:/home/ubuntu/upwork/upwork-0.0.1.jar
 ssh -i ~/.ssh/JobScraperKey.pem ubuntu@server-host "sudo systemctl restart upwork.service"
