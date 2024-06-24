1c8c2a9c2583705460519e21f930ded45edc4eed
Aaron Isaac <aaron@aisaac.co>
2024-06-25 05:09:06 +0700

    Add some additional filtering

--- src/main/java/co/aisaac/upwork/main_page/WebController.java
+++ src/main/java/co/aisaac/upwork/main_page/WebController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -32,9 +33,12 @@ public class WebController {
                     && job.status.equalsIgnoreCase(filter.status);
         }).toList();
 
+        if (Objects.equals(filter.status, "declined")) {
+            if (jobs.size() > 100)
+                jobs = jobs.subList(0, Math.min(100, jobs.size() - 1));
+        }
 
         // add a filter to this find all method
-
         model.addAttribute("filter", filter);
         model.addAttribute("jobs", jobs);
 
@@ -67,88 +71,7 @@ public class WebController {
         return ResponseEntity.ok().build();
     }
 
-//	private List<Job> filterJobs(MainFilter mainFilter, List<Job> all) {
-//
-//		// filter status
-//		String status = mainFilter.getStatus();
-//		List<Job> filtered = new ArrayList<>(all);
-//		filtered = filtered.stream().filter(job -> job.status.trim().equalsIgnoreCase(status.trim())).toList();
-//
-//		// filter job site
-//		String jobSite = mainFilter.getJobSite();
-//		if (!jobSite.equals("ALL")) {
-//			filtered = filtered.stream().filter(job -> job.job_site.trim().equalsIgnoreCase(jobSite.trim())).toList();
-//		}
-//
-//		// filter company
-//		List<String> companies = Arrays.stream(mainFilter.companySearch.split(",")).filter(s -> !s.isBlank()).toList();
-//		if (!companies.isEmpty()) {
-//			filtered = filtered.stream().filter(job -> {
-//				for (String company : companies)
-//					if (job.company.trim().toLowerCase(Locale.ROOT).contains(company.trim().toLowerCase(Locale.ROOT)))
-//						return true;
-//
-//				return false;
-//			}).toList();
-//		}
-//
-//		// filter search term
-//		List<String> searchTerms = Arrays.stream(mainFilter.searchTerms.split(",")).filter(s -> !s.isBlank()).toList();
-//		if (!searchTerms.isEmpty()) {
-//			filtered = filtered.stream().filter(job -> {
-//				var desc = job.description.toLowerCase();
-//				for (String term : searchTerms) {
-//					if (desc.contains(term.trim().toLowerCase())) return true;
-//					if (job.title.toLowerCase(Locale.ROOT).contains(term.trim().toLowerCase())) return true;
-//					if (job.subtitle.toLowerCase(Locale.ROOT).contains(term.trim().toLowerCase())) return true;
-//				}
-//				return false;
-//			}).toList();
-//		}
-//
-//		// filter title
-//		List<String> titleSearchTerms = Arrays.stream(mainFilter.titleSearch.split(",")).filter(s -> !s.isBlank()).toList();
-//		if (!titleSearchTerms.isEmpty()) {
-//			filtered = filtered.stream().filter(job -> {
-//				for (String term : titleSearchTerms) {
-//					if (job.title.toLowerCase(Locale.ROOT).contains(term.trim().toLowerCase())) return true;
-//				}
-//				return false;
-//			}).toList();
-//		}
-//
-//
-//		 highlight search term
-//		highlight(searchTerms, filtered, "highlight");
-//
-//		 sort
-//		filtered = filtered.stream().sorted((o1, o2) -> o1.company.compareToIgnoreCase(o2.company)).toList();
-//
-//		return filtered;
-//	}
-//
-//	private void augmentCompanies(List<Job> jobs) {
-//		// add statuses to each company
-//
-//		Map<String, Set<String>> comps = new HashMap<>();
-//
-//		for (Job job : jobs) {
-//			var comp = job.company;
-//			var status = job.status;
-//			if (comps.containsKey(comp)) {
-//				comps.get(comp).add(status);
-//			} else {
-//				comps.put(comp, new HashSet<>());
-//				comps.get(comp).add(status);
-//			}
-//		}
-//
-//		for (Job job : jobs) {
-//			var statuses = comps.get(job.company);
-//			job.companyStatuses = statuses.stream().toList();
-//		}
-//	}
-//
+
 //	private void highlight(List<String> searchTerms, List<Job> filtered, String className) {
 //		if (searchTerms.isEmpty()) return;
 //		if (filtered.isEmpty()) return;
